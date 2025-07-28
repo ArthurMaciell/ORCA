6518483f680d603c8eb4c94d0630ef6c359dea86
Arthur Maciel <arthur6325@gmail.com>
2025-07-28 14:00:19 -0300

    configurando o MLFLOW TRACKING no main.py

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from ORCA.pipeline.stage_02_data_validation import DataValidationTrainingPipelin
 from ORCA.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from ORCA.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from ORCA.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+import os
+import mlflow
+
+# Autenticação e configuração do MLflow remoto (DagsHub)
+os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/ArthurMaciell/ORCA.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"] = "ArthurMaciell"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "9ba42c98aca839c1595d1086f91d6985efa8d261"
+
+mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
+mlflow.set_experiment("IA_Orcamento")  # ou outro nome que desejar
 
 
 if __name__ == '__main__':
